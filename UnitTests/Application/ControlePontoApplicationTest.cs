9ef9d084cbd988ba08beeca2e4c5e6d0d5d73b7f
Ricardo Alexandre <ricardo.alexandresa@outlook.com>
2023-04-09 12:41:22 -0300

    adicao dos testes unitarios

---
+++ UnitTests/Application/ControlePontoApplicationTest.cs
@@ -0,0 +1,39 @@
+﻿using Application.ControlePonto;
+using Application.ControlePonto.Models;
+using Domain.ControlePonto.Command;
+using MediatR;
+using Moq.AutoMock;
+
+namespace UnitTests.Application;
+
+public class ControlePontoApplicationTest
+{
+    private readonly IControlePontoApplication _controlePontoApplication;
+    private readonly AutoMocker _autoMocker;
+
+    public ControlePontoApplicationTest(IControlePontoApplication controlePontoApplication)
+    {
+        _autoMocker = new AutoMocker();
+        _controlePontoApplication = controlePontoApplication;
+    }
+
+
+    [Fact]
+    public void RegistrarPontoComSucesso()
+    {
+        //Arrange
+        var momentoModel = new MomentoModel
+        {
+            DataHora = "2018-08-22T08:00:00"
+        };
+        var registrarPontoCommand = new RegistrarPontoCommand(DateTime.Parse(momentoModel.DataHora));
+
+        _autoMocker.Setup<IMediator>(x => x.Send(registrarPontoCommand, CancellationToken.None));
+
+        //Act
+        _controlePontoApplication.RegistrarPonto(momentoModel);
+
+        //Assert
+        
+    }
+}
